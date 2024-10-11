03ec111650ea2aa63f23643fd885a6d08eebc06d
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-11 18:50:50 +0100

    learn angular 1 - components in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  template: `
+    Hello Universe
+  `,
+  styles: `
+    :host {
+      color: #a144eb;
+    }
+  `,
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-playground';
-}
+
+export class AppComponent {}
