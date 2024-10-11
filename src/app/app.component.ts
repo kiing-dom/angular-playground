b40265d5136b4609c302cbca7d9b0e403a6e9169
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-11 20:01:54 +0100

    learn angular 6property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   template: `
-    @for(user of users; track user.id) {
-    {{ user.name }}
-    }
+    <div [contentEditable]="isEditable"></div>
   `,
-  standalone: true,
+  standalone: true
 })
+
 export class AppComponent {
-  users = [
-    { id: 0, name: 'Sarah' },
-    { id: 1, name: 'Amy' },
-    { id: 2, name: 'Rachel' },
-    { id: 3, name: 'Jessica' },
-    { id: 4, name: 'Poornima' },
-  ];
+  isEditable = true;
 }
