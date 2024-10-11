47fa9b48fbf006f33dfc358efff1aabf7a5c6b37
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-11 19:34:33 +0100

    learn angular 4 control flow if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
-  selector: 'app-user',
+  selector: 'app-root',
   template: `
-    Username: {{ username }}
+    @if(isServerRunning) {
+    <span>Yes, the server is running</span>
+    }
+    @else {
+      <span>No, the server is not running</span>
+    }
   `,
   standalone: true,
 })
-export class UserComponent {
-  username = '1KiingDom'
-}
-
-@Component({
-  selector: 'app-root',
-  template: `<section><app-user /></section>`,
-  imports: [UserComponent],
-  standalone: true,
-})
-
 export class AppComponent {
-  city: string = 'San Francisco' // can omit the type because of typescript's type inference
+  isServerRunning: boolean = true;
 }
