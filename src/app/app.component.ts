2daa80ba90204af0f8a628b38c2fc53185e96cc7
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-11 19:40:55 +0100

    learn angular 5 control flow for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,18 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   template: `
-    @if(isServerRunning) {
-    <span>Yes, the server is running</span>
-    }
-    @else {
-      <span>No, the server is not running</span>
+    @for(user of users; track user.id) {
+    {{ user.name }}
     }
   `,
   standalone: true,
 })
 export class AppComponent {
-  isServerRunning: boolean = true;
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
 }
