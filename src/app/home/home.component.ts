8d76f0df225062bc87b46d598f10fbd7a2ab0f2c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-12 09:17:57 +0100

    learned to define a route

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-home',
+    template: `
+        <div>Home Page</div>
+    `,
+})
+
+export class HomeComponent {}
