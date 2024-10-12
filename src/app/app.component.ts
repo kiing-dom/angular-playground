12fda9620680e437069d18ffa817b0d0c88973b3
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-12 09:10:40 +0100

    learn angular 12 - enable routing using Angular Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,19 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common'
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
   template: `
-    <p>Username: {{ username }}</p>
-    <p>Preferred Framwork: </p>
-    <ul>
-      <li>
-        Static Image:
-        <img ngSrc="/favicon.ico" alt="Icon" width="32" height="32" priority />
-      </li>
-      <li>
-        Dynamic Image:
-        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
-      </li>
-    </ul>
+    <nav>
+      <a href="/">Home</a>
+      |
+      <a href="/user">User</a>
+    </nav>
   `,
-  imports: [NgOptimizedImage],
+  standalone: true,
+  imports: [RouterOutlet]
 })
 
 export class AppComponent {
-  logoUrl: string = '/favicon.ico';
-  logoAlt: string = 'Logo';
-  username: string = '1KIINGDOM';
+  
 }
