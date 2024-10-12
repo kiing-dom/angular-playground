46a26709250ee8621cee72da58e8655faacfe155
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-12 17:42:53 +0100

    learn angular 17 - reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { Component } from '@angular/core';
-import {RouterOutlet, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <nav>
-      <a routerLink="/">Home</a>
-      |
-      <a routerLink="/user">User</a>
-    </nav>
-    <router-outlet />
-  `,
   standalone: true,
-  imports: [RouterOutlet, RouterLink]
+  template: `
+    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
+      <label
+        >Name
+        <input type="text" formControlName="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" formControlName="email" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+
+    <h2>Profile Form</h2>
+    <p>Name: {{ profileForm.value.name }}</p>
+    <p>Email: {{ profileForm.value.email }}</p>
+  `,
+  imports: [ReactiveFormsModule],
 })
-
 export class AppComponent {
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
 
+  handleSubmit() {
+    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
 }
