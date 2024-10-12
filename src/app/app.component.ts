a726bd7f93ac97e26d12d001840674c058fa08db
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-12 19:35:58 +0100

    learn angular 24 create a pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
-import { Component } from '@angular/core';
-import {DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
+import { Component } from "@angular/core";
+import { ReversePipe } from "./pipes/reverse.pipe";
 
 @Component({
   selector: 'app-root',
-  template: `
-    <ul>
-      <li>Number with "decimal" {{ num | number:"3.2-2" }} </li>
-      <li>Date with "date" {{ birthday | date: "medium"  }} </li>
-      <li>Currency with "currency" {{ cost | currency:"EUR" }} </li>
-    </ul>
-  `,
   standalone: true,
-  imports: [DecimalPipe, DatePipe, CurrencyPipe]
+  template: ` Reverse Machine: {{ word | reverse }} `,
+  imports: [ReversePipe],
 })
 
-export class AppComponent{
-  num = 103.1234;
-  birthday = new Date(2023, 3, 2);
-  cost = 4560.34;
+export class AppComponent {
+  word = "moD gniiK ot ebircsbuS";
 }
