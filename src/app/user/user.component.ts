c971ea8cf6de57367821d9d46dbeaf7be44f9a00
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-10-12 13:36:03 +0100

    learn angular 15 - form control values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,15 +5,20 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   selector: 'app-user',
   template: `
-    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <p>Framework: {{ favoriteFramework }} </p>
     <label for="framework">
         Favorite Framework:
         <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
     </label>
+    <button (click)="showFramework()">Show Framework</button>
   `,
   imports: [FormsModule],
 })
 export class UserComponent {
   username: string = '@KIINGDOM';
   favoriteFramework: string = '';
+
+  showFramework() {
+    alert(this.favoriteFramework)
+  }
 }
